5f4504211764b90a3d770e83fa54bcf025ec65d3
Blair Leduc <BlairLeduc@users.noreply.github.com>
2023-07-23 18:33:58 +0100

    Fix rename

--- src/providers/rename.ts
+++ src/providers/rename.ts
@@ -12,27 +12,44 @@ export class RenameProvider implements vscode.RenameProvider {
     cancellationToken: vscode.CancellationToken): Promise<vscode.WorkspaceEdit | undefined> {
 
     if (!cancellationToken.isCancellationRequested) {
-      const assemblyDocument = this.workspaceManager
-        .getAssemblyDocument(document, cancellationToken);
-
       const symbolManager = this.workspaceManager.getSymbolManager(document);
 
-      if (assemblyDocument && symbolManager) {
-        const assemblyLine = assemblyDocument.lines[position.line];
-        const reference = assemblyLine.references
-          .find(r => r.range.contains(position)) ?? assemblyLine.label;
-        
-        if (reference && reference.uri && reference.range.contains(position)) {
-          const references = symbolManager.references.filter(r => r.text === reference.text);
-          const symbol = symbolManager.implementations.find(i => i.text === reference.text);
+      if (symbolManager) {
+        const edit = new vscode.WorkspaceEdit();
 
-          if (symbol && symbol.uri) {
-            const edit = new vscode.WorkspaceEdit();
-            edit.replace(symbol.uri, symbol.range, newName);
-            references.forEach(s => edit.replace(s.uri, s.range, newName));
-            return edit;
+        // First, check if the user is renaming a label
+        const implementation = symbolManager.implementations
+          .find(i => i.range.contains(position));
+
+        if (implementation) {
+          edit.replace(implementation.uri, implementation.range, newName);
+
+          symbolManager.references
+            .filter(r => r.text === implementation.text
+              && r.blockNumber === implementation.blockNumber)
+            .forEach(s => edit.replace(s.uri, s.range, newName));
+        } else {
+          // Next, check if the user is renaming a reference
+          const reference = symbolManager.references
+            .find(r => r.range.contains(position));
+
+          if (reference) {
+            const implementation = symbolManager.implementations
+              .find(i => i.text === reference.text
+                && i.blockNumber === reference.blockNumber);
+
+            if (implementation) {
+              edit.replace(implementation.uri, implementation.range, newName);
+            }
+
+            symbolManager.references
+              .filter(r => r.text === reference.text
+                && r.blockNumber === reference.blockNumber)
+              .forEach(s => edit.replace(s.uri, s.range, newName));
           }
         }
+
+        return edit;
       }
     }
   }
@@ -41,21 +58,24 @@ export class RenameProvider implements vscode.RenameProvider {
     document: vscode.TextDocument,
     position: vscode.Position,
     cancellationToken: vscode.CancellationToken)
-      : Promise<vscode.Range | { range: vscode.Range; placeholder: string; } | undefined> {
+    : Promise<vscode.Range | { range: vscode.Range; placeholder: string; } | undefined> {
 
     if (!cancellationToken.isCancellationRequested) {
-      const assemblyDocument = this.workspaceManager
-        .getAssemblyDocument(document, cancellationToken);
-      
       const symbolManager = this.workspaceManager.getSymbolManager(document);
 
-      if (assemblyDocument && symbolManager) {
-        const assemblyLine = assemblyDocument.lines[position.line];
-        const symbol = assemblyLine.references
-          .find(r => r.range.contains(position)) ?? assemblyLine.label;
+      if (symbolManager) {
+        const implementation = symbolManager.implementations
+          .find(r => r.range.contains(position));
 
-        if (symbol && symbol.range.contains(position)) {
-          return { range: symbol.range, placeholder: symbol.text };
+        if (implementation) {
+          return { range: implementation.range, placeholder: implementation.text };
+        }
+
+        const reference = symbolManager.references
+          .find(r => r.range.contains(position));
+        
+        if (reference) {
+          return { range: reference.range, placeholder: reference.text };
         }
       }
     }
