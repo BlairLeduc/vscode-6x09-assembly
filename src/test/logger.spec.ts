fa25d58e2debd93c54746c9279e0f4259975a7c3
Blair Leduc <BlairLeduc@users.noreply.github.com>
2023-08-06 17:49:19 +0100

    Fix the unit test for Logger

--- src/test/logger.spec.ts
+++ src/test/logger.spec.ts
@@ -3,9 +3,11 @@ import { Logger } from '../logger';
 
 describe('Logger', () => {
   describe('no channel passed to init', () => {
+    it('should not be initialized', () => {
+      expect(Logger.isInitialized).toBeFalsy();
+    });
     it('should create a channel', () => {
       Logger.init();
-
       expect(Logger.isInitialized).toBeTruthy();
     });
   });
@@ -16,27 +18,22 @@ describe('Logger', () => {
     });
     it('should log trace messages', () => {
       Logger.trace('test');
-
       expect(logOutputChannel.trace).toBeCalledWith('test');
     });
     it('should log debug messages', () => {
       Logger.debug('test');
-
       expect(logOutputChannel.debug).toBeCalledWith('test');
     });
     it('should log info messages', () => {
       Logger.info('test');
-
       expect(logOutputChannel.info).toBeCalledWith('test');
     });
     it('should log warn messages', () => {
       Logger.warn('test');
-
       expect(logOutputChannel.warn).toBeCalledWith('test');
     });
     it('should log error messages', () => {
       Logger.error('test');
-
       expect(logOutputChannel.error).toBeCalledWith('test');
     });
   });
